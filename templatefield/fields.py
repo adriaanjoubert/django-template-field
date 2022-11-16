76d994e7f9a824de5712529623e1b458fbdf761e
Ivan Rostovsky <ivan.rostovsky@gmail.com>
2022-11-16 16:36:48 +0200

    Delete context from the `from_db_value`.

--- templatefield/fields.py
+++ templatefield/fields.py
@@ -5,12 +5,12 @@ from .settings import get_setting
 
 class TemplateTextField(models.TextField):
 
-    def from_db_value(self, value, expression, connection, context):
+    def from_db_value(self, value, expression, connection):
         if value is None:
             return None
 
         should_render = get_setting('TEMPLATE_FIELD_RENDER')
-        if not context.get('show_rendered') or not should_render:
+        if not should_render:
             # `show_rendered` flag is unset, return the templeate
             return value
 
@@ -18,5 +18,4 @@ class TemplateTextField(models.TextField):
         template = Template(value)
         context_dict = {}
         context_dict.update(get_setting('TEMPLATE_FIELD_CONTEXT'))
-        context_dict.update(context.get('tmpl_context', {}))
         return template.render(Context(context_dict))
